5f6e6c155d2446b962aff18ef13530b8ad26038d
kengurenokRu <nastassiadunaeva@gmail.com>
2024-01-24 12:55:27 +0300

    feat: add solution create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+    fs.access(path.resolve(__dirname, './files/fresh.txt'), fs.constants.R_OK | fs.constants.W_OK, function (err) {
+        if (!err) console.log("FS operation failed!");
+        else {
+            const file = fs.createWriteStream(path.resolve(__dirname, './files/fresh.txt'), (err) => {
+                if (err) console.log("FS operation failed!");
+            });
+            file.write("I am fresh and young");
+            file.close();
+        }
+    });
 };
 
 await create();
