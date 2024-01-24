f0fadcd9cd94418f988d38cb3f139a666fbd7631
kengurenokRu <nastassiadunaeva@gmail.com>
2024-01-24 13:21:30 +0300

    feat: add solution copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,7 +1,29 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import * as url from 'url';
 
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const oldPath = path.resolve(__dirname, "./files");
+const newPath = path.resolve(__dirname, "./files_copy");
 
 const copy = async () => {
- 
-}
+    fs.readdir(oldPath, (err, files) => {
+        if (err) console.log('FS operation failed');
+        else {
+            fs.mkdir(newPath, err => {
+                if (err) console.log('FS operation failed');
+                else {
+                    files.forEach(file => {
 
+                        fs.copyFile(path.resolve(oldPath, file), path.resolve(newPath, file), (err) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                    });
+                }
+            });
+        }
+    });  
+}
 await copy();
