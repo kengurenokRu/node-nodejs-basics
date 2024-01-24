267c1b99b464bc225118b479c6ba0e4432a164d8
kengurenokRu <nastassiadunaeva@gmail.com>
2024-01-24 17:25:42 +0300

    feat: add solution rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+    fs.access(path.resolve(__dirname, './files/properFilename.md'), fs.constants.R_OK, function (err) {
+        if (!err) console.log("FS operation failed!");
+        else {
+            fs.access(path.resolve(__dirname, './files/wrongFilename.txt'), fs.constants.R_OK, function (err) {
+                if (err) console.log("FS operation failed!");
+                else
+                {
+                    fs.rename(path.resolve(__dirname, './files/wrongFilename.txt'), path.resolve(__dirname, './files/properFilename.md'), err => {
+                        if(err) console.log("FS operation failed!");                        
+                     });
+                }
+            });     
+        }
+    });
 };
 
 await rename();
