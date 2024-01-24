a539b932054d6691adaf2decd2c9102c21af2b12
kengurenokRu <nastassiadunaeva@gmail.com>
2024-01-24 17:18:20 +0300

    feat: add solution delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,12 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const remove = async () => {
-    // Write your code here 
+    fs.unlink(path.resolve(__dirname, "./files/fileToRemove.txt"), (err) => {
+        if (err) throw 'FS operation failed';
+      });
 };
 
 await remove();
