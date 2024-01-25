4e36c46cbd7e343c0f83c23d405a547ca72405d7
kengurenokRu <nastassiadunaeva@gmail.com>
2024-01-25 16:59:15 +0300

    feat: add solution hash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import * as sha256 from 'node:crypto';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const file = new fs.createReadStream(path.resolve(__dirname, "./files/fileToCalculateHashFor.txt"));
+
 const calculateHash = async () => {
-    // Write your code here 
+    file.on('data', async function (data) {
+        const hash = sha256.createHash('sha256');
+        let temp = hash.update(data, 'utf-8');
+        console.log(temp.digest('hex'));
+    });
 };
 
 await calculateHash();
