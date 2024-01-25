f6b6b108ec83dd8bbe57d5d237b6a0e2a7d1c2c6
kengurenokRu <nastassiadunaeva@gmail.com>
2024-01-25 15:11:23 +0300

    feat: add solution args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,8 @@
 const parseArgs = () => {
-    // Write your code here 
+    let argv = process.argv.slice(2);
+    for (let k = 0; k <= argv.length - 1; k += 2) {
+        console.log(argv[k].slice(2) + " is " + argv[k + 1]);
+    }
 };
 
 parseArgs();
